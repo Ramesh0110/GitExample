3dde84833b296c3e83d623a676735af9722d0779
rames <rames@192.168.0.16>
2020-04-25 21:36:15 -0400

    Adding another sysout and // comment

--- GitHubProject2/src/com/learning/git/tests/HelloWorld.java
+++ GitHubProject2/src/com/learning/git/tests/HelloWorld.java
@@ -3,8 +3,9 @@ package com.learning.git.tests;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+		//comments
 		System.out.println("Hello world !");
+		System.out.println("GitHub learning is more interesting");
 		
 		
 	}
